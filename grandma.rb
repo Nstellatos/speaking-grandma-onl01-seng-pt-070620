fc02b03868858680fd911e12560343b35bd07571
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-09 01:08:39 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -4,7 +4,7 @@ def speak_to_grandma(phrase)
   end
   if phrase != phrase.uppercase()
     return "HUH?! SPEAK UP SONNY!"
-  
+  end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
