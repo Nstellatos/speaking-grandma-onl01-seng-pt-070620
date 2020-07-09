52161a80362dc8bc5336acbbdeaf3655cbd4b129
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-09 01:23:17 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,11 +1,11 @@
 def speak_to_grandma(phrase)
   if phrase = "I LOVE YOU GRANDMA!"
-    return "I LOVE YOU TOO PUMPKIN!"
+    puts "I LOVE YOU TOO PUMPKIN!"
   elsif
   if phrase != phrase.uppercase()
-    return "HUH?! SPEAK UP SONNY!"
+    puts "HUH?! SPEAK UP SONNY!"
   elsif
-    return "NO, NOT SINCE 1938!"
+    puts "NO, NOT SINCE 1938!"
   end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
