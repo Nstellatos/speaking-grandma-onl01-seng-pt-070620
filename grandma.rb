306a5de3f4f03efe21ae84761e838b8d77acbf95
Nicole Stellatos <stellatosnicole@gmail.com>
2020-07-09 01:38:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
+  end
   elsif
   phrase == phrase.downcase()
     puts "HUH?! SPEAK UP SONNY!"
